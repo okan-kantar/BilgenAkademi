bdbb84fbdd52d1b231471e214b59e33dfd64e253
okan-kantar <okkant@gmail.com>
2026-03-12 22:45:42 +0300

    up

--- 2026-1-nodejs-bootcamp-main/config/corsConfig.js
+++ 2026-1-nodejs-bootcamp-main/config/corsConfig.js
@@ -15,7 +15,7 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeader: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
